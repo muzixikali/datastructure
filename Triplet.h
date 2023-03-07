92f2d1390fba07b9db16bbe99f0a0e71bebda3b1
muzixikali <3468485032@qq.com>
2023-03-07 23:37:38 +0800

    初始化

---
+++ Triplet.h
@@ -0,0 +1,11 @@
+#include<datastructure.h>
+
+// Triplet 类型别名
+typedef ElemType* Triplet;
+
+// 函数原型
+
+Status InitTriplet(Triplet &T,ElemType v1,ElemType v2,ElemType v3);
+// 构造三元组
+
+Status DestoryTriplet(Triplet )
---
+++ datastructure.h
@@ -0,0 +1,13 @@
+// 状态常量
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+// 状态码
+typedef int Status;
+// 元素类型
+typedef int ElemType;
+
